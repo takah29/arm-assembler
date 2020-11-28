92ab797be02ecbc88750ffef44b665f4ef09dc3c
takah29 <takah0209@gmail.com>
2020-11-29 04:39:46 +0900

    strip関数追加

--- assembler/src/main.cpp
+++ assembler/src/main.cpp
@@ -5,6 +5,17 @@
 
 using namespace std;
 
+std::string strip(std::string& s, const std::string trim_str) {
+    std::string result;
+
+    std::string::size_type left = s.find_first_not_of(trim_str);
+    if (left != std::string::npos) {
+        std::string::size_type right = s.find_last_not_of(trim_str);
+        result = s.substr(left, right - left + 1);
+    }
+    return result;
+}
+
 string asmcode_to_bits(const string asmcode) { return asmcode; }
 
 int main(int argc, char** argv) {
