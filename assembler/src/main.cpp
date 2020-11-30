259336a1d84c7025560fc1b6f6f8f37f69392620
takah29 <takah0209@gmail.com>
2020-11-30 23:12:51 +0900

    strip関数削除

--- assembler/src/main.cpp
+++ assembler/src/main.cpp
@@ -3,18 +3,9 @@
 #include <string>
 #include <vector>
 
+#include "utility.hpp"
+
 using namespace std;
-
-std::string strip(std::string& s, const std::string trim_str) {
-    std::string result;
-
-    std::string::size_type left = s.find_first_not_of(trim_str);
-    if (left != std::string::npos) {
-        std::string::size_type right = s.find_last_not_of(trim_str);
-        result = s.substr(left, right - left + 1);
-    }
-    return result;
-}
 
 vector<string> tokenize(const string asmcode) {
     vector<string> tokens{};
