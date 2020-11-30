1835023587d32118e2af1da07e0c83033409ceca
takah29 <takah0209@gmail.com>
2020-11-30 23:29:49 +0900

    不要なコードを削除

--- assembler/src/main.cpp
+++ assembler/src/main.cpp
@@ -3,49 +3,10 @@
 #include <string>
 #include <vector>
 
+#include "assembly.hpp"
 #include "utility.hpp"
 
 using namespace std;
-
-vector<string> tokenize(const string asmcode) {
-    vector<string> tokens{};
-    size_t left_pos = 0, right_pos = 0;
-    bool in_brackets = false;
-
-    for (size_t i = 1; i < asmcode.size(); i++) {
-        if (in_brackets == true) continue;
-
-        if (asmcode[i] == ' ') {
-            if (asmcode[i - 1] == ',') {
-                right_pos = left_pos = i;
-            } else if (asmcode[i - 1] != ' ') {
-                right_pos = i;
-                tokens.push_back(asmcode.substr(left_pos, right_pos - left_pos));
-                left_pos = i;
-            }
-        } else if (asmcode[i] == '[' or asmcode[i] == '{') {
-            in_brackets = true;
-            left_pos = right_pos = i;
-        } else if (asmcode[i] == ']' or asmcode[i] == '}') {
-            in_brackets = false;
-            right_pos = i;
-            tokens.push_back(asmcode.substr(left_pos, right_pos - left_pos + 1));
-            left_pos = i;
-        } else if (asmcode[i] == ',') {
-            right_pos = i;
-            tokens.push_back(asmcode.substr(left_pos, right_pos - left_pos));
-            left_pos = i;
-        } else if (asmcode[i] != ' ' and asmcode[i - 1] == ' ') {
-            left_pos = right_pos = i;
-        }
-    }
-
-    tokens.push_back(asmcode.substr(left_pos, asmcode.size() - left_pos + 1));
-
-    return tokens;
-}
-
-string asmcode_to_bits(const string asmcode) { return asmcode; }
 
 int main(int argc, char **argv) {
     std::vector<std::string> args(argv, argv + argc);
