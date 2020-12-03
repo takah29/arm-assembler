c218f3fbeadf5b41fec738baa4a50a5486457326
takah29 <takah0209@gmail.com>
2020-12-03 23:04:20 +0900

    各命令のメンバ関数を追加(未実装) + その他変更

--- assembler/src/assembler.cpp
+++ assembler/src/assembler.cpp
@@ -1,21 +1,24 @@
 #include "assembler.hpp"
 
+#include <algorithm>
 #include <string>
 #include <vector>
 
-Assembler::Assembler() {
-    dataproc_insts = {{"AND", "EOR", "SUB", "RSB", "ADD", "ADC", "SBC", "RSC", "TST",
-                       "TEQ", "CMP", "CMN", "ORR", "MOV", "LSL", "LSR", "ASR", "RRX",
-                       "ROR", "BIC", "MVN", "MUL", "MLA", "UMU", "UML", "SMU", "SML"}};
-    memory_insts = {{"STR", "LDR"}};
-    branch_insts = {{"B", "BL"}};
-    branch_instructions = {{"B", "BL"}};
-}
+Assembler::Assembler() {}
 Assembler::~Assembler() {}
 
 std::string Assembler::convert(const std::string asmcode) {
     auto tokens = tokenize(asmcode);
+
     std::string opcode = tokens[0];
+    auto opcode_ = opcode.substr(0, 3);
+    std::vector<std::string> operands(tokens.begin() + 1, tokens.end());
+
+    if (opcode_ == "and") {
+        _and(opcode, operands);
+    } else if (opcode_ == "eor") {
+        _and(opcode_, operands);
+    }
 
     return asmcode;
 }
@@ -57,3 +60,36 @@ std::vector<std::string> Assembler::tokenize(const std::string asmcode) {
 
     return tokens;
 }
+
+/* Data Processing Instructions */
+uint32_t Assembler::_and(std::string opcode, std::vector<std::string> operands) { return 0; };
+uint32_t Assembler::_eor(std::string opcode, std::vector<std::string> operands) { return 0; };
+uint32_t Assembler::_sub(std::string opcode, std::vector<std::string> operands) { return 0; };
+uint32_t Assembler::_rsb(std::string opcode, std::vector<std::string> operands) { return 0; };
+uint32_t Assembler::_add(std::string opcode, std::vector<std::string> operands) { return 0; };
+uint32_t Assembler::_adc(std::string opcode, std::vector<std::string> operands) { return 0; };
+uint32_t Assembler::_sbc(std::string opcode, std::vector<std::string> operands) { return 0; };
+uint32_t Assembler::_rsc(std::string opcode, std::vector<std::string> operands) { return 0; };
+uint32_t Assembler::_tst(std::string opcode, std::vector<std::string> operands) { return 0; };
+uint32_t Assembler::_teq(std::string opcode, std::vector<std::string> operands) { return 0; };
+uint32_t Assembler::_cmp(std::string opcode, std::vector<std::string> operands) { return 0; };
+uint32_t Assembler::_cmn(std::string opcode, std::vector<std::string> operands) { return 0; };
+uint32_t Assembler::_orr(std::string opcode, std::vector<std::string> operands) { return 0; };
+uint32_t Assembler::_mov(std::string opcode, std::vector<std::string> operands) { return 0; };
+uint32_t Assembler::_lsl(std::string opcode, std::vector<std::string> operands) { return 0; };
+uint32_t Assembler::_lsr(std::string opcode, std::vector<std::string> operands) { return 0; };
+uint32_t Assembler::_asr(std::string opcode, std::vector<std::string> operands) { return 0; };
+uint32_t Assembler::_rrx(std::string opcode, std::vector<std::string> operands) { return 0; };
+uint32_t Assembler::_ror(std::string opcode, std::vector<std::string> operands) { return 0; };
+uint32_t Assembler::_bic(std::string opcode, std::vector<std::string> operands) { return 0; };
+uint32_t Assembler::_mvn(std::string opcode, std::vector<std::string> operands) { return 0; };
+uint32_t Assembler::_mul(std::string opcode, std::vector<std::string> operands) { return 0; };
+uint32_t Assembler::_mla(std::string opcode, std::vector<std::string> operands) { return 0; };
+
+/* Memory Instructions*/
+uint32_t Assembler::_str(std::string opcode, std::vector<std::string> operands) { return 0; };
+uint32_t Assembler::_ldr(std::string opcode, std::vector<std::string> operands) { return 0; };
+
+/* Branch Instructions */
+uint32_t Assembler::_b(std::string opcode, std::vector<std::string> operands) { return 0; };
+uint32_t Assembler::_bl(std::string opcode, std::vector<std::string> operands) { return 0; };
