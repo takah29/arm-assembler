aec68730d85dce157614fca3b7427b8212f1e8de
takah29 <takah0209@gmail.com>
2020-12-03 23:03:36 +0900

    各命令のメンバ関数を定義 + 微修正

--- assembler/src/assembler.hpp
+++ assembler/src/assembler.hpp
@@ -1,5 +1,4 @@
 #include <string>
-#include <unordered_map>
 #include <vector>
 
 enum class InstructionType { DataProcessing, Memory, Branch };
@@ -12,9 +11,38 @@ class Assembler {
     std::string convert(const std::string asmcode);
 
    private:
-    std::array<std::string, 27> dataproc_insts;
-    std::array<std::string, 2> memory_insts;
-    std::array<std::string, 2> branch_insts;
-
     std::vector<std::string> tokenize(const std::string asmcode);
+
+    /* Data Processing Instructions */
+    uint32_t _and(std::string opcode, std::vector<std::string> operands);
+    uint32_t _eor(std::string opcode, std::vector<std::string> operands);
+    uint32_t _sub(std::string opcode, std::vector<std::string> operands);
+    uint32_t _rsb(std::string opcode, std::vector<std::string> operands);
+    uint32_t _add(std::string opcode, std::vector<std::string> operands);
+    uint32_t _adc(std::string opcode, std::vector<std::string> operands);
+    uint32_t _sbc(std::string opcode, std::vector<std::string> operands);
+    uint32_t _rsc(std::string opcode, std::vector<std::string> operands);
+    uint32_t _tst(std::string opcode, std::vector<std::string> operands);
+    uint32_t _teq(std::string opcode, std::vector<std::string> operands);
+    uint32_t _cmp(std::string opcode, std::vector<std::string> operands);
+    uint32_t _cmn(std::string opcode, std::vector<std::string> operands);
+    uint32_t _orr(std::string opcode, std::vector<std::string> operands);
+    uint32_t _mov(std::string opcode, std::vector<std::string> operands);
+    uint32_t _lsl(std::string opcode, std::vector<std::string> operands);
+    uint32_t _lsr(std::string opcode, std::vector<std::string> operands);
+    uint32_t _asr(std::string opcode, std::vector<std::string> operands);
+    uint32_t _rrx(std::string opcode, std::vector<std::string> operands);
+    uint32_t _ror(std::string opcode, std::vector<std::string> operands);
+    uint32_t _bic(std::string opcode, std::vector<std::string> operands);
+    uint32_t _mvn(std::string opcode, std::vector<std::string> operands);
+    uint32_t _mul(std::string opcode, std::vector<std::string> operands);  // MUL, UMULL, SMULL
+    uint32_t _mla(std::string opcode, std::vector<std::string> operands);  // MLA, UMLAL, SMLAL
+
+    /* Memory Instructions*/
+    uint32_t _str(std::string opcode, std::vector<std::string> operands);
+    uint32_t _ldr(std::string opcode, std::vector<std::string> operands);
+
+    /* Branch Instructions */
+    uint32_t _b(std::string opcode, std::vector<std::string> operands);
+    uint32_t _bl(std::string opcode, std::vector<std::string> operands);
 };
