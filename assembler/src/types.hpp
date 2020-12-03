7881da1e858bbdccc04548991e231e99d6544a52
takah29 <takah0209@gmail.com>
2020-12-03 23:02:23 +0900

    整数型を定義

---
+++ assembler/src/types.hpp
@@ -0,0 +1,15 @@
+#ifndef _TYPES_H
+#define _TYPES_H
+
+#include <cstdint>
+
+using std::int8_t;
+using std::uint8_t;
+using std::int16_t;
+using std::uint16_t;
+using std::int32_t;
+using std::uint32_t;
+using std::int64_t;
+using std::uint64_t;
+
+#endif
