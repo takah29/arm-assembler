f82725d37eedd5bf86c15e4e40253f47a77b7518
takah29 <takah0209@gmail.com>
2020-12-11 00:22:53 +0900

    汎用プリント関数追加

--- assembler/src/utility.hpp
+++ assembler/src/utility.hpp
@@ -1,7 +1,27 @@
 #ifndef _UTILITY_H
 #define _UTILITY_H
 
+#include <iostream>
 #include <string>
+
+template <class... A>
+void print() {
+    std::cout << std::endl;
+}
+template <class... A>
+void prints_rest() {
+    std::cout << std::endl;
+}
+template <class T, class... A>
+void prints_rest(const T &first, const A &... rest) {
+    std::cout << " " << first;
+    prints_rest(rest...);
+}
+template <class T, class... A>
+void print(const T &first, const A &... rest) {
+    std::cout << first;
+    prints_rest(rest...);
+}
 
 std::string strip(std::string &s, const std::string trim_str);
 
