cdd2fd56c7e69e9568db18d6a5e16b3e7813ab65
takah29 <takah0209@gmail.com>
2020-12-11 00:52:41 +0900

    コマンドライン引数にデバッグ用フラグを指定できるようにした

--- assembler/src/main.cpp
+++ assembler/src/main.cpp
@@ -12,13 +12,14 @@ using namespace std;
 int main(int argc, char **argv) {
     std::vector<std::string> args(argv, argv + argc);
 
-    if (argc != 2) {
+    if (argc < 2 or 3 < argc) {
         printf("Error: Wrong number of arguments.\n");
-        printf("Usage: assembler <textfile>");
+        printf("Usage: assembler <textfile> [option -d]");
         return 1;
     }
 
     string asmfile_path = args[1];
+    bool debug_flag = (args[2] == "-d") ? true : false;
     ifstream infile(asmfile_path);
 
     if (infile.fail()) {
@@ -33,7 +34,10 @@ int main(int argc, char **argv) {
         if (asmcode == "" or asmcode[0] == '#') {
             continue;
         }
-        auto machine_code = assembler.convert(asmcode);
+        auto machine_code = assembler.convert(asmcode, debug_flag);
+        if (debug_flag) {
+            cout << asmcode << " -> ";
+        }
         cout << bitset<32>(machine_code) << endl;
     }
     return 0;
