cb7122833a6c5f035d75f11bacfd81396a7bf00e
takah29 <takah0209@gmail.com>
2020-12-20 03:57:08 +0900

    テーブルを修正

--- assembler/src/assembler.hpp
+++ assembler/src/assembler.hpp
@@ -14,7 +14,7 @@ class Assembler {
     uint32_t convert(std::string asmcode, const bool debug_flag);
 
    private:
-    inline static const std::unordered_map<std::string, std::unordered_map<std::string, uint32_t>> opcode_info_map{
+    inline static const std::unordered_map<std::string, std::unordered_map<std::string, uint32_t>> opcodebase_info{
         {"and", {{"ftype", 1}, {"cmd", 0b0000}}},
         {"eor", {{"ftype", 1}, {"cmd", 0b0001}}},
         {"sub", {{"ftype", 1}, {"cmd", 0b0010}}},
@@ -38,18 +38,20 @@ class Assembler {
         {"mvn", {{"ftype", 1}, {"cmd", 0b1111}}},
         {"mul", {{"ftype", 5}, {"cmd", 0b000}}},
         {"mla", {{"ftype", 6}, {"cmd", 0b001}}},
-        {"umu", {{"ftype", 6}, {"cmd", 0b100}}},
-        {"uml", {{"ftype", 6}, {"cmd", 0b101}}},
-        {"smu", {{"ftype", 6}, {"cmd", 0b110}}},
-        {"sml", {{"ftype", 6}, {"cmd", 0b111}}},
+        {"umull", {{"ftype", 6}, {"cmd", 0b100}}},
+        {"umlal", {{"ftype", 6}, {"cmd", 0b101}}},
+        {"smull", {{"ftype", 6}, {"cmd", 0b110}}},
+        {"smlal", {{"ftype", 6}, {"cmd", 0b111}}},
         {"str", {{"ftype", 7}, {"L", 0b0}}},
         {"ldr", {{"ftype", 7}, {"L", 0b1}}},
         {"b",   {{"ftype", 8}, {"L", 0b0}}},
         {"bl",  {{"ftype", 8}, {"L", 0b1}}}};
-    inline static const std::unordered_map<std::string, uint32_t> cond_map{
+    inline static const std::unordered_map<std::string, uint32_t> cond_info{
         {"eq", 0b0000}, {"ne", 0b0001}, {"cs", 0b0010}, {"hs", 0b0010}, {"cc", 0b0011}, {"lo", 0b0011},
         {"mi", 0b0100}, {"pl", 0b0101}, {"vs", 0b0110}, {"vc", 0b0111}, {"hi", 0b1000}, {"ls", 0b1001},
         {"ge", 0b1010}, {"lt", 0b1011}, {"gt", 0b1100}, {"le", 0b1101}, {"al", 0b1110}};
+    std::unordered_map<std::string, std::unordered_map<std::string, uint32_t>> opcode_info;
+
 
     std::vector<std::string> tokenize(const std::string asmcode);
     std::vector<std::string> split_operands(const std::string operandes, const size_t n_operands);
