ef0cd0f26309df4cc869e52b1ded69666b569802
takah29 <takah0209@gmail.com>
2020-12-20 17:53:20 +0900

    OpcodeInfo型を定義

--- assembler/src/types.hpp
+++ assembler/src/types.hpp
@@ -2,6 +2,7 @@
 #define _TYPES_H
 
 #include <cstdint>
+#include <unordered_map>
 
 using std::int8_t;
 using std::uint8_t;
@@ -11,5 +12,7 @@ using std::int32_t;
 using std::uint32_t;
 using std::int64_t;
 using std::uint64_t;
+
+using OpcodeInfo = std::unordered_map<std::string, std::unordered_map<std::string, uint32_t>>;
 
 #endif
