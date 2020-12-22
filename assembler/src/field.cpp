3e0920697895516f3eb44672251d5310f9194358
takah29 <takah0209@gmail.com>
2020-12-22 22:00:21 +0900

    レジスタの別名を追加

--- assembler/src/field.cpp
+++ assembler/src/field.cpp
@@ -6,6 +6,14 @@ Field::Field(OpcodeInfo *opcode_info) : opcode_info(opcode_info) {}
 Field::~Field() {}
 
 uint32_t Field::get_reg_4bit(const std::string reg) const {
+    if (reg == "pc") {
+        return 15;
+    } else if (reg == "lr") {
+        return 14;
+    } else if (reg == "sp") {
+        return 13;
+    }
+
     uint32_t reg_num = std::stoi(reg.substr(1, 2));
     if (reg_num < 16) {
         return reg_num;
