06f9481fc0d68af03e9f72edc9928f3ff7dded0d
takah29 <takah0209@gmail.com>
2020-12-26 02:23:01 +0900

    FormatType1, 2, 3の命令のテストを追加

---
+++ assembler/test/test_assembler.cpp
@@ -0,0 +1,54 @@
+
+#include <gtest/gtest.h>
+
+#include <bitset>
+#include <fstream>
+
+#include "../src/assembler.hpp"
+#include "../src/utility.hpp"
+
+using namespace std;
+
+class AssemblerTest : public ::testing::Test {
+   protected:
+    virtual void SetUp() { assembler_ptr = new Assembler{}; }
+    virtual void TearDown() { delete assembler_ptr; }
+
+    void assert_vec_eq(vector<string> input_data, vector<string> expected_data) {
+        for (auto [input, expected] : zip(input_data, expected_data)) {
+            if (input == "" and expected == "") break;
+            auto asmcode = unit_space(strip(input, " "));
+            print(input, "->");
+            auto result = assembler_ptr->convert(asmcode, false);
+            ASSERT_EQ(result, std::stoul(expected, nullptr, 2));
+        }
+    }
+
+    Assembler *assembler_ptr;
+};
+
+TEST_F(AssemblerTest, correct_type1_input_test) {
+    auto input_data = loadtxt("input/input_field_type_1.txt");
+    auto expected_data = loadtxt("input/input_field_type_1_ans.txt");
+    assert_vec_eq(input_data, expected_data);
+}
+
+TEST_F(AssemblerTest, correct_type2_input_test) {
+    auto input_data = loadtxt("input/input_field_type_2.txt");
+    auto expected_data = loadtxt("input/input_field_type_2_ans.txt");
+
+    assert_vec_eq(input_data, expected_data);
+}
+
+TEST_F(AssemblerTest, correct_type3_input_test) {
+    auto input_data = loadtxt("input/input_field_type_3.txt");
+    auto expected_data = loadtxt("input/input_field_type_3_ans.txt");
+
+    assert_vec_eq(input_data, expected_data);
+}
+
+TEST_F(AssemblerTest, correct_type4_input_test) {
+    auto input_data = loadtxt("input/input_field_type_4.txt");
+    auto expected_data = loadtxt("input/input_field_type_4_ans.txt");
+    assert_vec_eq(input_data, expected_data);
+}
